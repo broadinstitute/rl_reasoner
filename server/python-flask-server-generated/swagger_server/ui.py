b1f0d23bdab84baabdec4e00b5ea09fd0b66aacd
Mathias Wawer <mwawer@broadinstitute.org>
2019-09-09 18:34:59 -0700

    Update ui.py

--- server/python-flask-server-generated/swagger_server/ui.py
+++ server/python-flask-server-generated/swagger_server/ui.py
@@ -1,15 +1,15 @@
 import datetime
 
 from rl_reasoner.query import Query
-##from openapi_server.models.response import Response  # noqa: E501
-from openapi_server.models.message import Message
-from openapi_server.models.node import Node
-from openapi_server.models.edge import Edge
-from openapi_server.models.node_attribute import NodeAttribute
-from openapi_server.models.edge_attribute import EdgeAttribute
-from openapi_server.models.knowledge_graph import KnowledgeGraph
-from openapi_server.models.result import Result
-from openapi_server.models.message_terms import MessageTerms
+##from swagger_server.models.response import Response  # noqa: E501
+from swagger_server.models.message import Message
+from swagger_server.models.node import Node
+from swagger_server.models.edge import Edge
+from swagger_server.models.node_attribute import NodeAttribute
+from swagger_server.models.edge_attribute import EdgeAttribute
+from swagger_server.models.knowledge_graph import KnowledgeGraph
+from swagger_server.models.result import Result
+from swagger_server.models.message_terms import MessageTerms
 
 
 def query(query_graph):
