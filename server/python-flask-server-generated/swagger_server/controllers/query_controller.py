67db012857b85af2c660c4f7891a4e1124b46f74
Mathias Wawer <mwawer@broadinstitute.org>
2019-09-10 09:57:57 -0700

    Update query_controller.py

--- server/python-flask-server-generated/swagger_server/controllers/query_controller.py
+++ server/python-flask-server-generated/swagger_server/controllers/query_controller.py
@@ -6,7 +6,7 @@ from swagger_server import util
 
 from swagger_server import ui
 
-def query(body):  # noqa: E501
+def query(request_body):  # noqa: E501
     """Query reasoner via one of several inputs
 
      # noqa: E501
