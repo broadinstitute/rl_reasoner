91bc908f155c32a6c7fc188a60794f621d9c7b6d
Mathias Wawer <mwawer@broadinstitute.org>
2019-09-10 12:20:46 -0700

    Update ui.py

--- server/python-flask-server/openapi_server/ui.py
+++ server/python-flask-server/openapi_server/ui.py
@@ -9,7 +9,7 @@ from openapi_server.models.edge import Edge
 #from openapi_server.models.edge_attribute import EdgeAttribute
 from openapi_server.models.knowledge_graph import KnowledgeGraph
 from openapi_server.models.result import Result
-from openapi_server.models.message_terms import MessageTerms
+#from openapi_server.models.message_terms import MessageTerms
 
 
 def query(query_graph):
